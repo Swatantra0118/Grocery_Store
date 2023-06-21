24145f66385a4e2b045f5e2b281ba84b191d6e4d
swatantra <swatantratiwari057@gmail.com>
2023-06-21 14:11:45 +0530

    HomePageCommit

--- GroceryStore/Areas/Customer/Controllers/HomeController.cs
+++ GroceryStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Store.Data.Repository.IRepository;
 using Store.Models;
 using System.Diagnostics;
 
@@ -8,15 +9,23 @@ namespace GroceryStore.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly  IUnitOfWork _unitOfOfWork;//dependency injection
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product= _unitOfOfWork.Product.Get(u=>u.Id==productId,includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
